67ec223c6a45fa6e9535f001edc168612bf6e3e1
Yuaihen <706375160@qq.com>
2021-01-26 16:29:01 +0800

    Update ApiService.java

--- app/src/main/java/com/example/retrofitlib/retrofit/api/ApiService.java
+++ app/src/main/java/com/example/retrofitlib/retrofit/api/ApiService.java
@@ -13,7 +13,6 @@ import retrofit2.http.POST;
  * Created by Yuaihen.
  * on 2019/7/5
  * 管理所有接口以及BaseUrl
- * 拦截器中已经默认添加参数Mac/Token/Version
  */
 public interface ApiService {
 
@@ -23,14 +22,6 @@ public interface ApiService {
     @POST("api/getToken")
     Call<Result<TokenBean>> getToken();
 
-    /**
-     * 获取轮播图
-     */
-    @POST("api/getAdInfo")
-    Call<Result<AdInfoBean>> getAdInfo();
-
-    @FormUrlEncoded
-    @POST("api/getConfig")
-    Call<Result<AdInfoBean>> getConfi(@Field("config") String config);
+  
 
 }
