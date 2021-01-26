6a64aa67380dd9707efc2d51395382717c8eca8f
Yuaihen <706375160@qq.com>
2021-01-26 16:26:50 +0800

    Update ResponseBodyConverter.java

--- app/src/main/java/com/example/retrofitlib/retrofit/converter/ResponseBodyConverter.java
+++ app/src/main/java/com/example/retrofitlib/retrofit/converter/ResponseBodyConverter.java
@@ -26,8 +26,6 @@ public class ResponseBodyConverter<T> implements Converter<ResponseBody, T> {
     private static final int SUCCESS = 0;                   // 成功
     private static final int FAILURE = -1;                  // 失败
     public static final int TOKEN_NOT_EXIST = 10086;        //token不存在
-    public static final int SERVER_EXCEPTION = 502;         //服务器异常
-    public static final int MAC_NOT_EXIST = 999;            //mac地址不存在，未入库
 
 
     public ResponseBodyConverter(Gson gson, TypeAdapter<T> adapter) {
@@ -60,10 +58,6 @@ public class ResponseBodyConverter<T> implements Converter<ResponseBody, T> {
                     throw new MyException("errorCode:" + FAILURE + " 服务器返回异常~");
                 case TOKEN_NOT_EXIST:
                     throw new MyException("errorCode:" + TOKEN_NOT_EXIST + " token参数不存在~");
-                case SERVER_EXCEPTION:
-                    throw new MyException("errorCode:" + SERVER_EXCEPTION + " 镜子后台正在维护升级中，请您稍后再试~");
-                case MAC_NOT_EXIST:
-                    throw new MyException("errorCode:" + MAC_NOT_EXIST + " Mac地址不存在，该设备未入库~");
                 default:
                     throw new MyException("IO异常，请重试~");
             }
